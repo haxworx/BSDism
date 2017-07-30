6524932faa6da9038f6ff4313da7e420d08d42e8
Al Poole <netstar@gmail.com>
2017-07-30 19:36:32 +0100

    refactor

--- bsd_generic_sensors.c
+++ bsd_generic_sensors.c
@@ -154,16 +154,13 @@ void _memsize_bytes_to_kb(unsigned long *bytes)
 #define _memsize_kb_to_mb _memsize_bytes_to_kb
 
 
-static void bsd_cpuinfo(int ncpu,
-                        unsigned long *prev_total,
-                        unsigned long *prev_idle,
-                        unsigned int  *prev_percent,
-                        cpu_core_t    **cores)
+static void bsd_cpuinfo(cpu_core_t **cores, int ncpu)
 {
-
    size_t size;
-   unsigned long percent_all = 0, total_all = 0, idle_all = 0;
    int i, j;
+   int percent, diff_total, diff_idle;
+   double ratio;
+   unsigned long total, idle, used;
    cpu_core_t *core;
 #if defined(__FreeBSD__) || defined(__DragonFly__)
    if (!ncpu) return;
@@ -175,23 +172,23 @@ static void bsd_cpuinfo(int ncpu,
 
    for (i = 0; i < ncpu; i++)
      {
+        core = cores[i];
         unsigned long *cpu = cpu_times[i];
         double total = 0;
 
         for (j = 0; j < CPU_STATES; j++)
            total += cpu[j];
 
-        core = cores[i];
 
         int diff_total = total - core->total;
         int diff_idle = cpu[4] - core->idle;
 
         if (diff_total == 0) diff_total = 1;
 
-        double ratio = diff_total / 100.0;
+        ratio = diff_total / 100.0;
         unsigned long used = diff_total - diff_idle;
 
-        int percent = used / ratio;
+        percent = used / ratio;
         if (percent > 100)
            percent = 100;
         else if (percent < 0)
@@ -200,83 +197,77 @@ static void bsd_cpuinfo(int ncpu,
         core->percent = percent;
         core->total = total;
         core->idle = cpu[4];
-
-        percent_all += (int) percent;
-        total_all += total;
-        idle_all += core->idle;
      }
-   *prev_total = total_all / ncpu;
-   *prev_idle = idle_all / ncpu;
-   *prev_percent = (int) (percent_all / ncpu);
 #elif defined(__OpenBSD__)
+   unsigned long cpu_times[CPU_STATES];
    if (!ncpu) return;
    if (ncpu == 1)
      {
-        unsigned long cpu_times[CPU_STATES];
+        core = cores[0];
         int cpu_time_mib[] = { CTL_KERN, KERN_CPTIME };
         size = CPU_STATES * sizeof(unsigned long);
         if (sysctl(cpu_time_mib, 2, &cpu_times, &size, NULL, 0) < 0)
           return;
 
-        unsigned long total = 0;
+        total = 0;
         for (j = 0; j < CPU_STATES; j++)
           total += cpu_times[j];
 
-        unsigned long idle = cpu_times[4];
+        idle = cpu_times[4];
 
-        int diff_total = total - *prev_total;
-        int diff_idle = idle - *prev_idle;
+        diff_total = total - core->total;
+        diff_idle = idle - core->idle;
 
         if (diff_total == 0) diff_total = 1;
 
-        double ratio = diff_total / 100.0;
-        unsigned long used = diff_total - diff_idle;
-        int percent = used / ratio;
+        ratio = diff_total / 100.0;
+        used = diff_total - diff_idle;
+        percent = used / ratio;
         if (percent > 100)
           percent = 100;
         else if (percent < 0)
           percent = 0;
 
-        *prev_total = total;
-        *prev_idle = idle; // cpu_times[3];
-        *prev_percent = (int) percent;
+        core->percent = percent;
+        core->total = total;
+        core->idle = idle;
      }
    else if (ncpu > 1)
      {
         for (i = 0; i < ncpu; i++)
           {
-             unsigned long cpu_times[CPU_STATES];
-             size = CPU_STATES * sizeof(unsigned long);
+             core = cores[i];
              int cpu_time_mib[] = { CTL_KERN, KERN_CPTIME2, 0 };
+             size = CPU_STATES * sizeof(unsigned long);
              cpu_time_mib[2] = i;
              if (sysctl(cpu_time_mib, 3, &cpu_times, &size, NULL, 0) < 0)
                return;
 
-             unsigned long total = 0;
+             total = 0;
              for (j = 0; j < CPU_STATES - 1; j++)
                total += cpu_times[j];
 
-             core = cores[i];
-             int diff_total = total - core->total;
-             int diff_idle  = cpu_times[4] - core->idle;
+             idle = cpu_times[4];
 
-             core->total = total;
-             core->idle = cpu_times[4];
-             if (diff_total == 0) diff_total = 1;
-             double ratio = diff_total / 100;
-             unsigned long used = diff_total - diff_idle;
-             int percent = used / ratio;
-             if (percent > 100) percent = 100;
-             else if (percent < 0) percent = 0;
+             diff_total = total - core->total;
+             if (diff_total == 0)
+                diff_total = 1;
+
+             diff_idle = idle - core->idle;
+
+             ratio = diff_total / 100;
+             used = diff_total - diff_idle;
+             percent = used / ratio;
+
+             if (percent > 100)
+                percent = 100;
+             else if (percent < 0)
+                percent = 0;
+             
              core->percent = percent;
-
-             percent_all += (int) percent;
-             total_all += total;
-             idle_all += core->idle;
+             core->total = total;
+             core->idle = idle;
           }
-        *prev_total =  (total_all / ncpu);
-        *prev_idle =   (idle_all / ncpu);
-        *prev_percent = (percent_all / ncpu) + (percent_all % ncpu);
      }
 #endif
 }
@@ -285,8 +276,6 @@ static cpu_core_t **bsd_generic_cpuinfo(int *ncpu)
 {
    cpu_core_t **cores;
    int i;
-   unsigned int prev_perc = 0;
-   unsigned long prev_idle = 0, prev_total = 0;
    
    *ncpu = cpu_count();
 
@@ -295,9 +284,9 @@ static cpu_core_t **bsd_generic_cpuinfo(int *ncpu)
    for (i = 0; i < *ncpu; i++)
         cores[i] = calloc(1, sizeof(cpu_core_t));
 
-   bsd_cpuinfo(*ncpu, &prev_total, &prev_idle, &prev_perc, cores);
+   bsd_cpuinfo(cores, *ncpu);
    usleep(1000000);
-   bsd_cpuinfo(*ncpu, &prev_total, &prev_idle, &prev_perc, cores);
+   bsd_cpuinfo(cores, *ncpu);
    
    return cores;
 }
